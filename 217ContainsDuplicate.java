24c0c4bc7edc941bad5d3e6986fafa8242629e18
Juan  Cervantes <90647857+JuanCervantesAla@users.noreply.github.com>
2024-03-05 18:46:35 -0600

    Create 217ContainsDuplicate.java
    Final solution:
    7 ms in runtime
    61.18 MB in memory

---
+++ 217ContainsDuplicate.java
@@ -0,0 +1,24 @@
+class Solution {//Cervantes Alatorre Juan Jose Emiliano
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> conj = new HashSet<>();//Creating a HashSet for non-duplicated storing
+        
+        int counter = 0;
+        while(counter < nums.length){//Full check the array
+
+            boolean added = conj.add( nums[counter] );//If the function addesde is true it means the number
+            its not duplicated
+
+            if(!added){//If not added means there are duplicated numbers and returning a false
+                return true;
+            }
+
+            counter++;//Inc the counter
+
+        }
+
+        return false;//Otherwise means there were no duplicateds
+
+    }
+
+}
